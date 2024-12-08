6d8bbc8cf8095879ffe6c704772075af10bf88f7
Aidan Hopper <aidanhop1@gmail.com>
2024-12-07 21:02:35 -0800

    added hand wave

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
-
-	err := http.ListenAndServe(":9999", nil)
-	if err != nil {
-		log.Fatal(err)
+func serve() {
+	fmt.Println("Starting server on 9999")
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		fmt.Println("Server failed to start:", err)
 	}
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := filepath.Join("./public", r.URL.Path)
+		_, err := os.Stat(path)
+
+		// If the requested file exists, serve it
+    fmt.Println(err)
+		if err == nil {
+			http.ServeFile(w, r, path)
+			return
+		}
+
+		// Otherwise, serve index.html for React Router routes
+		http.ServeFile(w, r, filepath.Join("./public", "index.html"))
+	})
+
+  serve()
 }
